6fa05779d032582deb83973002ea54cb3db64164
thunyoubun <thunyoubun@gmail.com>
2023-01-17 04:14:03 +0700

    path quiz

--- pages/pages/myquizz.tsx
+++ pages/pages/myquizz.tsx
@@ -135,7 +135,7 @@ export default function Myquizz({ data }: any) {
                           </td>
                           <td className="p-2 gap-2 flex flex-row justify-center  align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
                             <Link
-                              href={"/admin/quiz/" + x.pathName}
+                              href={"/admin/quiz/" + x.subject}
                               className="hover:scale-105 text-base font-semibold leading-tight dark:text-green dark:opacity-80 text-white bg-gradient-to-r from-green-500 to-green-600  hover:bg-gradient-to-l hover:from-green-500 hover:to-green-600  p-4  tracking-wide  shadow-lg cursor-pointer transition ease-in duration-300 px-4 py-1 rounded-md "
                             >
                               <MdOutlineNotStarted size={25} />
