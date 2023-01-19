12ccda021897a342673e364e8278c200107bb415
thunyoubun <thunyoubun@gmail.com>
2023-01-19 14:04:14 +0700

    session properties

--- pages/pages/user.tsx
+++ pages/pages/user.tsx
@@ -165,7 +165,7 @@ const User = () => {
                       </div>
                     </div>
                     <button
-                      onClick={() => callPutTodo()}
+                      /* onClick={() => callPutTodo()} */
                       className="w-full flex justify-center bg-gradient-to-r from-indigo-500 to-blue-600  hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 p-4  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500"
                     >
                       SUBMIT
