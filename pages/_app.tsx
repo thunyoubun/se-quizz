7aaeba43af8e01cd4f40a85bd8f662d5c052636f
work2544 <work21_123@hotmail.com>
2023-02-15 19:37:36 +0700

    user cannot zoom out, a bit zoom in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,11 +7,18 @@ import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Session } from "inspector";
 import { ThemeProvider } from "../contexts/theme";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <ThemeProvider>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.25,user-scalable=0"
+          />
+        </Head>
         <Component {...pageProps} />
       </ThemeProvider>
     </AuthProvider>
