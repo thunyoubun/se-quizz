107e03e4b76681cd56cbf0742658682313d4671b
work2544 <work21_123@hotmail.com>
2023-02-15 19:27:28 +0700

    no zoom out

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-      <meta name="viewport" content="width=device-width , initial-scale=1,, maximum-scale=1.0, user-scalable=0"/>
+      <meta name="viewport" content="width=device-width , initial-scale=1,, maximum-scale=1.5, user-scalable=0"/>
       </Head>
       <body>
         <Main />
