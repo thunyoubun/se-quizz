07e0b938dd2f588aa92b2e703ee9741b691a3792
Switch <thunyoubun@gmail.com>
2023-03-14 22:27:05 +0700

    del group

--- pages/components/quizChoice/Group.tsx
+++ pages/components/quizChoice/Group.tsx
@@ -12,7 +12,7 @@ const group = [1, 2, 3, 4, 5];
 const Group = ({ id, value }: props) => {
   return (
     <>
-      <div className=" bg-white dark:bg-gray-600  rounded-md p-4 shadow-lg">
+      {/* <div className=" bg-white dark:bg-gray-600  rounded-md p-4 shadow-lg">
         <div className=" flex justify-between text-sm dark:text-white font-semibold">
           <h1>{id}. ปรนัย</h1>
           <div className="flex align-middle items-center dark:text-black gap-2 bg-slate-100 rounded-md p-2">
@@ -35,7 +35,7 @@ const Group = ({ id, value }: props) => {
             </div>
           );
         })}
-      </div>
+      </div> */}
     </>
   );
 };
