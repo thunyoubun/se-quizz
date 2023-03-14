9c0cf349284d956a063c2d9195b9561a90f5fd28
Switch <thunyoubun@gmail.com>
2023-03-15 00:45:55 +0700

    gg kub

--- pages/components/ChartQuiz.tsx
+++ pages/components/ChartQuiz.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
-function BarChart({ chartData }) {
+export declare type props = {
+  chartData: any;
+};
+
+const BarChart = ({ chartData }: props) => {
   return <Bar data={chartData} />;
-}
+};
 
 export default BarChart;
