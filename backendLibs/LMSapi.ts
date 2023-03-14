43989e0e464c330acbefbefb5aff1aa19eeef223
work2544 <work21_123@hotmail.com>
2023-03-15 00:00:59 +0700

    เปลี่ยนมาใช้axiosละ

--- backendLibs/LMSapi.ts
+++ backendLibs/LMSapi.ts
@@ -1,80 +1,99 @@
 import axios from "axios";
-
+type LMSparam = {
+  courseID: string | number;
+  quizID: string | number | null;
+  TOKEN: string;
+};
 type ClassicQuiz = {
-  courseID: string | number;
-  title: string;
-  TOKEN: string;
+  'quiz[title]': string;
 };
-type QuizGroup = {
-  courseID: string | number;
-  quizID:string | number;
-  name: string;
-  pickcount: number;
-  questionpoint: number;
-  TOKEN: string;
-};
-type Question = {
-  courseID: string | number;
-  quizID: string | number;
-  TOKEN: string;
-};
-export const CreateClassicQuiz = async (param: ClassicQuiz) => {
-  let response = await fetch(
-    `https://mango-cmu.instructure.com/api/v1/courses/${param.courseID}/quizzes?quiz[title]=${param.title}`,
-    {
-      method: "POST",
-      headers: {
-        Accept: "*/*",
-        Authorization: "Bearer " + param.TOKEN,
-      },
-    }
-  );
-
-  return response.json();
+type QuestionGroup = {
+  'quiz_groups[][name]': string;
+  'quiz_groups[][pick_count]': number;
+  'quiz_groups[][question_points]': number;
 };
 
-export const CreateQuizGroup = async (param:QuizGroup) => {
-  let response = await fetch(
-    `https://mango-cmu.instructure.com/api/v1/courses/${param.courseID}/quizzes/${param.quizID}/groups/?quiz_groups[][name]=${param.name}&quiz_groups[][pick_count]=${param.pickcount}&quiz_groups[][question_points]=${param.questionpoint}`,
-    {
-      method: "POST",
-      headers: {
-        Accept: "*/*",
-        Authorization: "Bearer " + param.TOKEN,
-      },
-    }
-  );
-
-  return response.json();
-};
-export const CreatQuestion = async (param:Question,body: unknown) => {
-  const {data} = await axios.post(`https://mango-cmu.instructure.com/api/v1/courses/${param.courseID}/quizzes/${param.quizID}/questions`,body, {
+export const CreateClassicQuiz = async (param: LMSparam, body: ClassicQuiz) => {
+  var options = {
+    method: 'POST',
+    url: `https://mango-cmu.instructure.com/api/v1/courses/${param.courseID}/quizzes`,
+    params: body,
     headers: {
-      Accept: "*/*",
-      Authorization: "Bearer " + param.TOKEN,
-    },
-  }
-)
-return {data}
+      Accept: '*/*',
+      Authorization: 'Bearer '+ param.TOKEN
+    }
+  };
+  const response =await  axios.request(options).then(res => {return res.data}).catch(function (error) {
+    console.error(error);
+  });
+  
+  return  response;
 };
 
+export const CreateQuizGroup = async (param: LMSparam, body: QuestionGroup) => {
+  var options = {
+    method: 'POST',
+    url: `https://mango-cmu.instructure.com/api/v1/courses/${param.courseID}/quizzes/${param.quizID}/groups/`,
+    params: body,
+    headers: {
+      Accept: '*/*',
+      Authorization: 'Bearer '+ param.TOKEN
+    }
+  };
+  const response =await  axios.request(options).then(res => {return res.data}).catch(function (error) {
+    console.error(error);
+  });
+  
+  return  response;
+};
+export const CreatQuestion = async (param: LMSparam, body: unknown) => {
+  var options = {
+    method: 'POST',
+    url: `https://mango-cmu.instructure.com/api/v1/courses/${param.courseID}/quizzes/${param.quizID}/questions`,
+    params: body,
+    headers: {
+      Accept: '*/*',
+      Authorization: 'Bearer '+ param.TOKEN
+    }
+  };
+  const response =await  axios.request(options).then(res => {return res.data}).catch(function (error) {
+    console.error(error);
+  });
+  
+  return  response;
+};
+
+const TOKEN =
+  "21123~Ci16eEjIuU2RnkZW4iPgSMq5cSOWgIiLUqFNdtUUCMaHhNFjSjMOb6IYRkHC62ZP";
+let quizID = 5159;
+let groupID:Array<{ quiz_group_id: string|number; quiz_group_name: string; }> = [{quiz_group_id:3690,quiz_group_name:'xxx'}];
 
 
+// CreateClassicQuiz(
+//   {
+//     courseID: "1306",
+//     quizID: null,
+//     TOKEN: TOKEN,
+//   },
+//   { 'quiz[title]': "via typescript" }
+// ).then((data) => {
+//   console.log(data);
+//   quizID = data.id;
+// });
 
-// const TOKEN ="21123~Ci16eEjIuU2RnkZW4iPgSMq5cSOWgIiLUqFNdtUUCMaHhNFjSjMOb6IYRkHC62ZP"
-// CreateClassicQuiz({
-//   courseID: "1306",
-//   title: "via typescript",
-//   TOKEN: TOKEN
-    
-// }).then((data) => console.log(data));
-// CreateQuizGroup({
-//   courseID: "1306",
-//   quizID:5150,
-//   name: "group from ts",
-//   pickcount: 1,
-//   questionpoint: 1, 
-//   TOKEN: TOKEN
-// }).then((data) => console.log(data));
 
-// CreatQuestion({courseID:1306,quizID:5150,TOKEN:TOKEN},{}).then((data) => console.log(data));
+// CreateQuizGroup(
+//   {
+//     courseID: "1306",
+//     quizID: quizID,
+//     TOKEN: TOKEN,
+//   },
+//   { 'quiz_groups[][name]': "group from ts", 'quiz_groups[][pick_count]': 1, 'quiz_groups[][question_points]': 1 }
+// ).then((data) => {
+//   console.log(data)
+//   groupID.push({'quiz_group_id':data.quiz_groups[0].id,'quiz_group_name':data.quiz_groups[0].name})
+// });
+
+CreatQuestion({ courseID: 1306, quizID: quizID, TOKEN: TOKEN }, {'question[quiz_group_id]': groupID[0].quiz_group_id,}).then((data) =>
+  console.log(data)
+);
